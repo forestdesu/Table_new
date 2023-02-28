f1f6bacbdde3551ce89fb4955dbd441ca3cad183
ivanovv <aa2652839@gmail.com>
2023-02-28 11:47:49 +0900

    123

--- app/src/main/java/com/example/table/MainActivity.java
+++ app/src/main/java/com/example/table/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.table;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView nav_view;
@@ -22,6 +29,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void init(){
         nav_view = findViewById(R.id.nav_view);
         nav_view.setNavigationItemSelectedListener(this);
+        //Test
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Tabla");
+        myRef.setValue("Hello, World!");
     }
 
     @Override
@@ -41,9 +52,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(this, "Pressed id smartphone", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_up:
+                signUpDialog(R.string.sign_up, R.string.sign_up_button);
                 Toast.makeText(this, "Pressed id sing up", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_in:
+                signUpDialog(R.string.sign_in, R.string.sign_in_button);
                 Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_out:
@@ -51,5 +64,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return true;
+    }
+
+    private void signUpDialog(int title, int buttonTitle) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
+        dialogBuilder.setView(dialogView);
+        TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+        titleTextView.setText(title);
+        Button b = dialogView.findViewById(R.id.buttonSignUp);
+        b.setText(buttonTitle);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
