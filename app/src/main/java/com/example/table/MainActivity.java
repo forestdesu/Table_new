309df5f8c0c337e5af9745da07bdb3731eb6226d
ivanovv <aa2652839@gmail.com>
2023-03-03 11:38:56 +0900

    123

--- app/src/main/java/com/example/table/MainActivity.java
+++ app/src/main/java/com/example/table/MainActivity.java
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView nav_view;
+    private FirebaseAuth mAuth;
+    private TextView userEmail;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +38,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void init(){
         nav_view = findViewById(R.id.nav_view);
+        userEmail = nav_view.getHeaderView(0).findViewById(R.id.tvEmail);
         nav_view.setNavigationItemSelectedListener(this);
         //Test
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         DatabaseReference myRef = database.getReference("Tabla");
-        myRef.setValue("Hello, World!");
+        myRef.setValue("Hello, World4!");
+    }
+
+    public void onStart() {
+        super.onStart();
+        getUserData();
+    }
+
+    private void getUserData() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            userEmail.setText(currentUser.getEmail());
+        } else {
+            userEmail.setText(R.string.sign_in_or_sign_up);
+        }
     }
 
     @Override
@@ -52,21 +78,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(this, "Pressed id smartphone", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_up:
-                signUpDialog(R.string.sign_up, R.string.sign_up_button);
+                signUpDialog(R.string.sign_up, R.string.sign_up_button, 0);
                 Toast.makeText(this, "Pressed id sing up", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_in:
-                signUpDialog(R.string.sign_in, R.string.sign_in_button);
+                signUpDialog(R.string.sign_in, R.string.sign_in_button, 1);
                 Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_out:
+                SignOut();
                 Toast.makeText(this, "Pressed id sign out", Toast.LENGTH_SHORT).show();
                 break;
         }
         return true;
     }
 
-    private void signUpDialog(int title, int buttonTitle) {
+    private void signUpDialog(int title, int buttonTitle, int index) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
@@ -75,13 +102,86 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         titleTextView.setText(title);
         Button b = dialogView.findViewById(R.id.buttonSignUp);
         b.setText(buttonTitle);
+        EditText edEmail = dialogView.findViewById(R.id.edEmail);
+        EditText edPassword = dialogView.findViewById(R.id.edPassword);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (index == 0) {
+                    singUp(edEmail.getText().toString(), edPassword.getText().toString());
+                } else {
+                    signIn(edEmail.getText().toString(), edPassword.getText().toString());
+                }
             }
         });
-        AlertDialog dialog = dialogBuilder.create();
+        dialog = dialogBuilder.create();
         dialog.show();
+    }
+
+    private void singUp(String email, String password) {
+        if (!email.equals("") && !password.equals(""))
+        {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if (user != null) {
+                                    Toast.makeText(getApplicationContext(), "SignUp done... user email: " + user.getEmail(),
+                                            Toast.LENGTH_SHORT).show();
+                                    Log.d("My log main activity", "signUpWithCustomToken:success " + user.getEmail());
+                                    getUserData();
+                                    dialog.dismiss();
+                                }
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("My log main activity", "signUpWithCustomToken:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Registration failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else {
+            Toast.makeText(this, "Email или пароль пустой", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void signIn(String email, String password) {
+        if (!email.equals("") && !password.equals(""))
+        {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if (user != null) {
+                                    Toast.makeText(getApplicationContext(), "SignIn done... user email: " + user.getEmail(),
+                                            Toast.LENGTH_SHORT).show();
+                                    Log.d("My log main activity", "signInWithCustomToken:success " + user.getEmail());
+                                    getUserData();
+                                    dialog.dismiss();
+                                }
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("My log main activity", "signInWithCustomToken:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else {
+            Toast.makeText(this, "Email или пароль пустой", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void SignOut(){
+        mAuth.signOut();
+        getUserData();
     }
 }
