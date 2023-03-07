994196487efb29ab14d0c0aa769de21450d4de5a
ivanovv <aa2652839@gmail.com>
2023-03-07 11:00:20 +0900

    123

--- app/src/main/java/com/example/table/EditActivity.java
+++ app/src/main/java/com/example/table/EditActivity.java
@@ -3,16 +3,26 @@ package com.example.table;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -23,6 +33,11 @@ import java.net.URI;
 public class EditActivity extends AppCompatActivity {
     private StorageReference mStorageRef;
     private ImageView imItem;
+    private Uri uploadUri;
+    private Spinner spinner;
+    private DatabaseReference dRef;
+    private FirebaseAuth mAuth;
+    private EditText edTitle, edPrice, edPhone, edDisc;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +45,24 @@ public class EditActivity extends AppCompatActivity {
         init();
     }
     private void init(){
+        edTitle = findViewById(R.id.edTitle);
+        edPhone = findViewById(R.id.edPhone);
+        edPrice = findViewById(R.id.edPrice);
+        edDisc = findViewById(R.id.edDescription);
         mStorageRef = FirebaseStorage.getInstance().getReference("Images");
         imItem = findViewById(R.id.imItem);
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 10 && data != null && data.getData() != null){
             if (resultCode == RESULT_OK) {
                 imItem.setImageURI(data.getData());
+                uploadImage();
             }
         }
     }
@@ -48,12 +73,27 @@ public class EditActivity extends AppCompatActivity {
         byte[] byteArray = out.toByteArray();
         final StorageReference mRef = mStorageRef.child(System.currentTimeMillis() + "_image");
         UploadTask up = mRef.putBytes(byteArray);
-        Task<URI> task = up.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<URI>>() {
+        Task<Uri> task = up.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
             @Override
-            public Task<URI> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                return null;
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return mRef.getDownloadUrl();
             }
-        })
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                uploadUri = task.getResult();
+                assert uploadUri != null;
+                Toast.makeText(EditActivity.this, "Upload done: " + uploadUri.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+    public void onClickSavePost(View view){
+
     }
     public void onClickImage(View view){
         getImage();
@@ -63,5 +103,19 @@ public class EditActivity extends AppCompatActivity {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, 10);
+    }
+    private void savePost(){
+        dRef = FirebaseDatabase.getInstance().getReference(spinner.getSelectedItem().toString());
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getUid() != null) {
+            NewPost post = new NewPost();
+            post.setImageId(uploadUri.toString());
+            post.setTitle(edTitle.getText().toString());
+            post.setPhone(edPhone.getText().toString());
+            post.setPrice(edPrice.getText().toString());
+            post.setDisc(edDisc.getText().toString());
+
+            //dRef.child(mAuth.getUid()).
+        }
     }
 }
